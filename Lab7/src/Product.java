2131774606b12e4a68d1f225ec147a3b6052b0a1
hyemin <khmin1121@ajou.ac.kr>
2017-05-01 11:02:45 +0900

    히이이이잌

---
+++ Lab7/src/Product.java
@@ -0,0 +1,18 @@
+
+public abstract class Product {
+	private  double price;
+	
+	
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	public abstract String getName();
+}
